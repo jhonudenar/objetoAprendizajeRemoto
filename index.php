bad4bdf72b7ed06f7976c192890b38b858cacaae
jhondominguez1 <jjjdomi@gmail.com>
2016-05-31 22:37:48 -0500

    nuevo php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Objeto Virtual de Aprendizaje</title>
+    </head>
+    <body>
+        <div style="width: 100%;">
+            <table style="width: 100%;">
+                <tr>
+                    <td style="width: 33%;">
+                        <a href="administrador.php">Administrador</a>
+                    </td>
+                    <td style="width: 33%;">
+                        <a href="docente.php">Docente</a>
+                    </td>
+                    <td style="width: 33%;">
+                        <a href="estudiante.php">Estudiante</a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
